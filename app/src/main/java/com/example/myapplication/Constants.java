09ce18bb6f501121c849f0f924d8ae02e0af8258
fangwenli <fangwenli@xiaomi.com>
2021-10-17 09:31:29 +0800

    提交未提交的

---
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+public class Constants {
+
+    public final String MODEL_NAME = "trafficLights.pt";
+
+    public static String[] IMAGENET_CLASSES = {"0", "1", "2"};
+
+
+}
